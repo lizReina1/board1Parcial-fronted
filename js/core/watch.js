f90be32890ea7c5b77b3ad1e87218ab663585216
lreina <lizfernanda.rq@gmail>
2024-01-19 23:48:37 -0400

    change code fro

--- js/core/watch.js
+++ js/core/watch.js
@@ -1,6 +1,6 @@
 'use strict'
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:9090');
+const socket = io.connect('https://board1-parcial-fronted.vercel.app/:9090');
 console.log('socket cargado...');
 
 module.exports =  {
