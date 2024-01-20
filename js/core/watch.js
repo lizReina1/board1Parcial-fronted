77ce34038d5a19abb6004973d5033d2af4fb8bbd
Dav <dav.trepsiuq>
2024-01-20 17:12:03 -0400

    Add ip socket IO

--- js/core/watch.js
+++ js/core/watch.js
@@ -1,7 +1,7 @@
 'use strict'
 const io = require('socket.io-client');
 
-const socket = io.connect('http://localhost:9090');
+const socket = io.connect('0.0.0.0:9090');
 console.log('socket cargado...');
 
 module.exports =  {
